4f5daf6c57c42c9f036b8345d442a6a765a16a64
AmienulRana <amienulrana@gmail.com>
2022-06-18 15:57:48 +0700

    menambahkan satu fungsi untuk melakukan pemanggilan api ke semua service agar tidak melakukan nya berulang-ulang

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,31 @@
 import axios from "axios";
-import { getToken } from "./storage";
+import { getToken, deleteToken } from './storage';
 const Token = "Bearer " + getToken();
 const UrlApi = "http://localhost:5000/";
+export const URL_API = 'http://localhost:5000';
+
+export const checkExpireToken = (response) => {
+    if(response.error && !response.auth){
+        deleteToken();
+       return window.location.href = '/login'
+    }
+    return true;
+}
+export const ServiceApi = async (method,path,data) => {
+    const response = await axios({
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        },
+        url: `${URL_API}/${path}`,
+        method: method,
+        data:data,
+    })
+    if(response && response.data){
+        if(checkExpireToken(response.data)){
+            return response.data;
+        }
+    }
+}
 export function getApi(url) {
   return axios({
     method: "get",
